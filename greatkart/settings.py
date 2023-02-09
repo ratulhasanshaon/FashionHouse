157dc1112128bde1161decf7504b4421618d1701
ratulhasanshaon <ratulhasanshaon@gmail.com>
2023-02-09 18:00:16 +0600

    for deployment

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
